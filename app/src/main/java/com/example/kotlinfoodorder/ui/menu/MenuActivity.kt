5b8671881ecc8900c9f102c52498d9b89dc4991d
Gabriel Simões <gabrielsimoest@gmail.com>
2024-11-08 22:40:39 -0300

    Feat: tela menu inicio

--- app/src/main/java/com/example/kotlinfoodorder/ui/menu/MenuActivity.kt
+++ app/src/main/java/com/example/kotlinfoodorder/ui/menu/MenuActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
-import com.example.kotlinfoodorder.databinding.ActivityLoginBinding
+import androidx.viewpager2.widget.ViewPager2
 import com.example.kotlinfoodorder.databinding.ActivityMenuBinding
 import com.example.kotlinfoodorder.ui.login.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MenuActivity : ComponentActivity() {
+
     private lateinit var binding: ActivityMenuBinding
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
@@ -20,7 +23,22 @@ class MenuActivity : ComponentActivity() {
         binding = ActivityMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener {
+        // Configuração do ViewPager2 e do Adapter
+        val viewPager: ViewPager2 = binding.viewPager
+//        val adapter = MenuPagerAdapter(this)
+//        viewPager.adapter = adapter
+
+        // Configuração do TabLayout com o ViewPager
+        val tabLayout: TabLayout = binding.tabLayout
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            when (position) {
+                0 -> tab.text = "Comidas"
+                1 -> tab.text = "Bebidas"
+            }
+        }.attach()
+
+        // Configuração do botão de logout
+        binding.logout.setOnClickListener {
             auth.signOut()
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
