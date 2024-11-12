87485d29ab929a588ac99699199063482ecee5d8
Gabriel Simões <gabrielsimoest@gmail.com>
2024-11-12 12:02:11 -0300

    Feat: cart view

--- app/src/main/java/com/example/kotlinfoodorder/ui/menu/MenuActivity.kt
+++ app/src/main/java/com/example/kotlinfoodorder/ui/menu/MenuActivity.kt
@@ -1,6 +1,7 @@
 package com.example.kotlinfoodorder.ui.menu
 
-import android.R
+import android.R.color
+import android.R.drawable
 import android.content.Intent
 import android.os.Bundle
 import android.widget.LinearLayout
@@ -8,13 +9,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlinfoodorder.databinding.ActivityMenuBinding
 import com.example.kotlinfoodorder.ui.login.LoginActivity
 import com.google.android.material.button.MaterialButton
-import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.auth
 
 
 class MenuActivity : ComponentActivity() {
@@ -34,17 +32,18 @@ class MenuActivity : ComponentActivity() {
         val userName = auth.currentUser?.displayName ?: "Usuário"
         binding.infoTextPrimary.text = "Olá, $userName"
 
+        initCartButtonListener()
         addFilterButtons()
 
         val myItemList = listOf(
-            Item("Pizza Margherita", "Pizza clássica com molho de tomate, mozzarella e manjericão.", 29.90, R.drawable.ic_menu_report_image),
-            Item("Hambúrguer", "Hambúrguer com carne, queijo, alface e molho especial.", 19.90, R.drawable.ic_menu_report_image),
-            Item("Sushi", "Sushi fresco com peixe de alta qualidade e arroz temperado.", 39.90, R.drawable.ic_menu_report_image),
-            Item("Espaguete à Bolonhesa", "Massa com molho de carne e tomate, servido com queijo parmesão.", 24.90, R.drawable.ic_menu_report_image),
-            Item("Coxinha", "Coxinha de frango empanada, crocante por fora e macia por dentro.", 8.90, R.drawable.ic_menu_report_image),
-            Item("Torta de Chocolate", "Torta de chocolate com recheio cremoso e cobertura de ganache.", 12.90, R.drawable.ic_menu_report_image),
-            Item("Caipirinha", "Bebida tradicional brasileira feita com cachaça, limão e açúcar.", 15.90, R.drawable.ic_menu_report_image),
-            Item("Mojito", "Cocktail refrescante com rum, hortelã, limão e soda.", 18.90, R.drawable.ic_menu_report_image)
+            Item("Pizza Margherita", "Pizza clássica com molho de tomate, mozzarella e manjericão.", 29.90, drawable.ic_menu_report_image),
+            Item("Hambúrguer", "Hambúrguer com carne, queijo, alface e molho especial.", 19.90, drawable.ic_menu_report_image),
+            Item("Sushi", "Sushi fresco com peixe de alta qualidade e arroz temperado.", 39.90, drawable.ic_menu_report_image),
+            Item("Espaguete à Bolonhesa", "Massa com molho de carne e tomate, servido com queijo parmesão.", 24.90, drawable.ic_menu_report_image),
+            Item("Coxinha", "Coxinha de frango empanada, crocante por fora e macia por dentro.", 8.90, drawable.ic_menu_report_image),
+            Item("Torta de Chocolate", "Torta de chocolate com recheio cremoso e cobertura de ganache.", 12.90, drawable.ic_menu_report_image),
+            Item("Caipirinha", "Bebida tradicional brasileira feita com cachaça, limão e açúcar.", 15.90, drawable.ic_menu_report_image),
+            Item("Mojito", "Cocktail refrescante com rum, hortelã, limão e soda.", 18.90, drawable.ic_menu_report_image)
         )
 
         val adapter = MenuAdapter(myItemList)
@@ -70,9 +69,16 @@ class MenuActivity : ComponentActivity() {
                 ).apply {
                     setMargins(16, 0, 16, 0)
                 }
-                setTextColor(ContextCompat.getColor(this@MenuActivity, android.R.color.white))
+                setTextColor(ContextCompat.getColor(this@MenuActivity, color.white))
             }
             binding.filterButtonsLayoutContainer.addView(button)
+        }
+    }
+
+    private fun initCartButtonListener() {
+        binding.cart.setOnClickListener {
+            val intent = Intent(this, CartActivity::class.java)
+            startActivity(intent)
         }
     }
 }
